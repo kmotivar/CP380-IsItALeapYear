61aa6d1061966631d8721513a67e92beec1611ad
kmotivar <kmotivar@confederationcollege.ca>
2021-07-28 16:19:32 -0400

    files added

---
+++ IsItALeapYear/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsItALeapYear(int year)
+        {
+            if (year % 4 != 0)
+            {
+                return false;
+            }
+            else if (year % 400 == 0)
+            {
+                return true;
+            }
+            else if (year % 100 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
